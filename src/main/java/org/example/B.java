0abd47daeafb6e98835f4e9ee9042ec8228cf445
dinelka <lakshan456DFG@QP7>
2024-01-23 22:02:48 +0530

    first commit second change

--- src/main/java/org/example/B.java
+++ src/main/java/org/example/B.java
@@ -4,5 +4,9 @@ public class B {
     public static void main(String[] args) {
         int [] ar = {1,2,3,4};
         System.out.println(ar[1]);
+
+        int x =20;
+        int y =30;
+        System.out.println(x+y);
     }
 }
