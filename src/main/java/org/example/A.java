d5f1bc4f78ee595196087c80006d6e0e4d596315
dinelka <lakshan456DFG@QP7>
2024-01-23 21:50:13 +0530

    first commit

---
+++ src/main/java/org/example/A.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class A {
+    public static void main(String[] args) {
+        for (int i=1; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/org/example/B.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class B {
+    public static void main(String[] args) {
+        int [] ar = {1,2,3,4};
+        System.out.println(ar[1]);
+    }
+}
